a45b6e16f61a21f6997da772949a4b5b4bf6647d
jaehyun <sjh121476@naver.com>
2023-11-23 17:02:38 +0900

    new_start

--- patient/admin.py
+++ patient/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
 
-from .models import Patient
+from .models import *
 
 # Register your models here.
 class PatientAdmin(admin.ModelAdmin):
     search_fields = ['name']
 admin.site.register(Patient, PatientAdmin)
+
+class Patient2Admin(admin.ModelAdmin):
+    search_fields = ['name']
+admin.site.register(Patient2, Patient2Admin)
+
+class ExercisePartAdmin(admin.ModelAdmin):
+    search_fields = ['part']
+admin.site.register(ExercisePart, ExercisePartAdmin)
+
+class ExerciseTypeAdmin(admin.ModelAdmin):
+    search_fields = ['type']
+admin.site.register(ExerciseType, ExerciseTypeAdmin)
+
+class ExerciseAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+admin.site.register(Exercise, ExerciseAdmin)
+
+class ExerciseListAdmin(admin.ModelAdmin):
+    search_fields = ['date']
+admin.site.register(ExerciseList, ExerciseListAdmin)
--- patient/models.py
+++ patient/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import uuid
+from django.utils import timezone
 
 # Create your models here.
 class Patient(models.Model):
@@ -6,4 +8,35 @@ class Patient(models.Model):
     name = models.CharField(max_length=10) # 이름
     front_resident = models.CharField(max_length=10) # 앞 주민번호
     back_resident = models.CharField(max_length=10) # 뒤 주민번호
-    phone = models.CharField(max_length=30) 
+    phone = models.CharField(max_length=30)
+
+#---------------------------------------------------------------------------------
+
+class Patient2(models.Model):
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4)
+    name = models.CharField(max_length=10)
+    front_resident = models.CharField(max_length=10)
+    back_resident = models.CharField(max_length=1)
+    phone = models.CharField(max_length=30)
+
+class ExercisePart(models.Model):
+    part = models.CharField(primary_key=True, max_length=20)
+
+class ExerciseType(models.Model):
+    type = models.CharField(primary_key=True, max_length=20)
+
+class Exercise(models.Model):
+    name = models.CharField(primary_key=True, max_length=30)
+    part = models.ForeignKey(ExercisePart, on_delete=models.CASCADE)
+    type = models.ForeignKey(ExerciseType, on_delete=models.CASCADE)
+
+class ExerciseList(models.Model):
+    code = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4)
+    patient = models.ForeignKey(Patient2, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    date = models.DateField(default=timezone.now)
+    count = models.IntegerField()
+    set = models.IntegerField()
+    time = models.IntegerField(default=0)
+    weight = models.IntegerField(default=0)
+
