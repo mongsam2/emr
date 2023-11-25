e07026a1c15d51f23b1f0a49be55ad98b5c4ccdb
jaehyun <sjh121476@naver.com>
2023-11-25 22:56:57 +0900

    exercise_list_dropdown

--- patient/models.py
+++ patient/models.py
@@ -30,6 +30,7 @@ class Exercise(models.Model): # 운동 설명영상 링크 저장
     name = models.CharField(primary_key=True, max_length=30)
     part = models.ForeignKey(ExercisePart, on_delete=models.CASCADE)
     type = models.ForeignKey(ExerciseType, on_delete=models.CASCADE)
+
 class ExerciseList(models.Model):
     code = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4)
     patient = models.ForeignKey(Patient2, on_delete=models.CASCADE)
